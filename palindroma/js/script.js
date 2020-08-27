381eec5a137260d080137761a658ec2ac06b1d0a
joseph <giuseppe.falco000@gmail.com>
2020-08-27 15:17:04 +0200

    add comment

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,19 +2,23 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+//richiesta parola
  var word = prompt("Inserisci una parola");
+//stampa parola
  document.getElementById("word-user").innerHTML += word;
- var str = word;
 
+//dichiarazione e assegnazione funzione
+ var str = word;
+ console.log(str);
+ str = checkPalindrom(str);
  console.log(str);
  
-str = checkPalindrom(str);
-console.log(str);
-
+ //funzione converti e inverti parola 
  function checkPalindrom (str) {
     return str == str.split('').reverse().join('');
   }
 
+  //controllo parola palindorma
  if ( str === true) {
      console.log("ok")
      document.getElementById("check-palindrome").innerHTML = "La parola è palindroma"
