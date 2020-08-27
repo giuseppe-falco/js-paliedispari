7f890be58bc9d8948dec6859ec0eec01c18f6d63
joseph <giuseppe.falco000@gmail.com>
2020-08-27 16:47:17 +0200

    .

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -6,18 +6,7 @@
 // Dichiariamo chi ha vinto.
 
 
-// funzione per scelta pari o dispari
-// function optionFunction() {
-//     var selector = document.getElementById("option-user");
-//     var option = selector[selector.selectedIndex].value;
 
-//     if ( option == "pari") {
-//         console.log("ciao")
-//     } else {
-//         console.log("puzzi")
-//     }
-
-// }
 
 var numberPc
 // funziona genera numero da 1 a 5
@@ -25,57 +14,55 @@ function randomNumberFunction(min, max) {
     numberPc = parseInt(document.getElementById("number-random").innerHTML = Math.floor(Math.random() * (max - min) + min));
 }
 
+
 //funzione massima
 function functionGame () {
     
-   // scelta numero da 1 a 5
-   var selectorNumber = document.getElementById("number-user");
-   var numberUser = selectorNumber[selectorNumber.selectedIndex].value;
-   numberUser = parseInt(numberUser);
-
-   //controllo se ha scelto un numero
-    if (numberUser == 0) {
-        alert("Non hai scelto alcun numero !")
-    }
-   //controllo se ha generato un numero
-    if (isNaN(numberPc)) {
-        alert("Non hai generato alcun numero !")
+    //scelta pari dispari
+    var selector = document.getElementById("option-user");
+    var option = selector[selector.selectedIndex].value;
+ 
+    if ( option == "pari") {
+        console.log("scelta utente pari");
+    } else {
+        console.log("scelta utente dispari")
     }
 
-   var add = numberUser + numberPc;
-   console.log(add)
+
+    // scelta numero da 1 a 5
+    var selectorNumber = document.getElementById("number-user");
+    var numberUser = selectorNumber[selectorNumber.selectedIndex].value;
+    numberUser = parseInt(numberUser);
+
+    //controllo se ha scelto un numero
+        if (numberUser == 0) {
+            alert("Non hai scelto alcun numero !")
+        }
+    //controllo se ha generato un numero
+        if (isNaN(numberPc)) {
+            alert("Non hai generato alcun numero !")
+        }
+
+    //somma
+    var add = numberUser + numberPc;
+    console.log(add)
 
 
+    //controllo somma pari/dispari
+        addCheck = "dispari";
+    if ( add % 2 == 0) {
+        console.log("somma pari")
+        addCheck = "pari";
+    } else {
+        console.log("somma dispari")
+    }
 
 
-   var selector = document.getElementById("option-user");
-   var option = selector[selector.selectedIndex].value;
+    //controllo vincitore
 
-
-   if ( option == "pari") {
-       console.log("scelta utente pari");
-   } else {
-       console.log("scelta utente dispari")
-   }
-
-
-
-
-   //controllo somma pari/dispari
-    addCheck = "dispari";
-   if ( add % 2 == 0) {
-       console.log("somma pari")
-       addCheck = "pari";
-   } else {
-       console.log("somma dispari")
-   }
-
-
-   //controllo vincitore
-
-   if ( addCheck == option ) {
-       console.log("HAI VINTO")
-   } else {
-       console.log("hai perso")
-   }
+    if ( addCheck == option ) {
+        console.log("HAI VINTO")
+    } else {
+        console.log("hai perso")
+    }
 }
